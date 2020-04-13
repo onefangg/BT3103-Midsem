f475449df804a14e629cd2d17e92f6e90279c168
WeiZheng <weizheng.lim97@gmail.com>
2020-04-14 00:14:45 +0800

    fix not signin

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ router.beforeEach(async (to, from, next) => {
   document.title = to.meta.title
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
   if (requiresAuth && !await firebase.getCurrentUser()){
-     next('Sign-In') 
+     next('NotSignedIn') 
   }
   else next()
 })
